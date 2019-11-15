bc355517f094bb7c8de5f8e25e807fec44076972
Vladimir-Simonyan <54622837+Vladimir-Simonyan@users.noreply.github.com>
2019-11-15 13:55:44 +0400

    Update Maze.cs

--- Maze.cs
+++ Maze.cs
@@ -1,4 +1,8 @@
-﻿using System;
+//   Copyright 2019 | Vladimir Simonyan | simonyan.vlad@gmail.com   // 
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
